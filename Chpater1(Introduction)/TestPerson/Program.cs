43e0d742c3baf1bbb3aa584bc324fca6186a0c4b
mohanadmjs <s1a@hotmail.no>
2020-09-28 12:06:30 +0300

    Chpter1

---
+++ Chpater1(Introduction)/TestPerson/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace The_Object_Oriented_Thought_Process_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Person joe =new Person();
+           joe.Name="joe";
+           Console.WriteLine(joe.Name);
+           Console.ReadLine();
+        }
+    }
+}
+
+public class Person
+{
+    //Attributes
+    private string strName;
+    private string strAddress;
+    
+    //Methods
+    public string Name
+    {
+        get{return strName;}
+        set{ strName = value;}
+    }
+    public string Address
+    {
+        get{return strAddress;}
+        set{strAddress=value;}
+    }
+}
---
+++ Chpater1(Introduction)/TestShape/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace TestShape
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle circle=new Circle(5);
+            Console.WriteLine(circle.calcArea());
+
+            Rectangle rectangle =new Rectangle(4, 5);
+            Console.WriteLine(rectangle.calcArea());
+
+            Console.ReadLine();
+
+        }
+    }
+    public abstract class Shape
+    {
+        protected double area;
+        public abstract double calcArea();
+    }
+
+    public class Circle : Shape
+    {
+        private double radius;
+        public Circle(double r)
+        {
+            radius = r;
+        }
+        public override double calcArea()
+        {
+            area = 3.14 * (radius * radius);
+            return (area);
+        }
+    }
+    public class Rectangle :Shape
+    {
+        private double lenght;
+        private double width;
+
+        public Rectangle(double l, double w)
+        {
+            lenght = l;
+            width = w;
+        }
+
+        public override double calcArea()
+        {
+            area =lenght *width;
+            return (area);
+        }
+    }
+
+
+}
